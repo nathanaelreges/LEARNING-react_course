bf457af822443857d67ac6fea8a3220634c1384e
Nathanael Reges <nathanaelregesdev@gmail.com>
2018-07-27 08:09:28 -0300

    Seventh exec. Class Component

--- src/Member.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Member = props => (
-   <div> { props.name } <strong> { props.lastName } </strong> </div>
-)
-
-export default Member
--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Family from './Family'
-import Member from './Member';
+import ClassComponent from './ClassComponent'
 
 
 ReactDOM.render(
-   <div>
-      <Family lastName="Silva">
-         <Member name="Pedro"></Member>
-         <Member name="João"></Member>
-         <Member name="Silvia"></Member>
-         <Member name="Natália"></Member>
-      </Family>
-   </div>
-   , document.querySelector('.app'))
+   <ClassComponent inner="Class Component" />
+   , document.querySelector('.app')
+)
 
